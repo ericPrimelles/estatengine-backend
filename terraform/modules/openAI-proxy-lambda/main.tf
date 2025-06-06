48a697ff7dcc554a491a39fd6b89be1714ea3782
ericPrimelles <eric.a.primelles96@gmail.com>
2025-06-06 01:16:35 -0400

    Fix on role reference

--- terraform/modules/openAI-proxy-lambda/main.tf
+++ terraform/modules/openAI-proxy-lambda/main.tf
@@ -57,7 +57,7 @@ resource "aws_iam_policy" "lambda_secrets" {
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_logs" {
-  role = aws_iam_role.openAI-proxy-role
+  role = aws_iam_role.openAI-proxy-role.arn
   policy_arn = aws_iam_policy.lambda_logging.arn
 }
 
