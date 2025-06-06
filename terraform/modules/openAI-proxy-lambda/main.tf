523bdf513381a0c2df13a4fc1abd1dbc54850e9d
ericPrimelles <eric.a.primelles96@gmail.com>
2025-06-06 01:27:39 -0400

    fix lambda role principal

--- terraform/modules/openAI-proxy-lambda/main.tf
+++ terraform/modules/openAI-proxy-lambda/main.tf
@@ -3,7 +3,7 @@ data "aws_iam_policy_document" "assume-role-policy" {
       effect = "Allow"
       principals {
         type = "Service"
-        identifiers = ["lambda.amazon.com"]
+        identifiers = ["lambda.amazonaws.com"]
       }
       actions = ["sts:AssumeRole"]
 
