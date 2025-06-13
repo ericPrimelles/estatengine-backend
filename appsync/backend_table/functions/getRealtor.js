2c6d39272b07557d1f8e02b28e8e35f139c027a6
ericPrimelles <eric.a.primelles96@gmail.com>
2025-06-13 19:42:30 -0400

    Operation getRealto

---
+++ appsync/backend_table/functions/getRealtor.js
@@ -0,0 +1,34 @@
+import {util} from "@aws-appsync/utils";
+
+/**
+ * Called before the request function of the first AppSync function in the pipeline.
+ *  @param ctx the context object holds contextual information about the function invocation.
+ */
+export function request(ctx) {
+    const PK = `REALTOR#ACCOUNT#${ctx.identity.username}#`;
+    const SK = `REALTOR#ACCOUNT#${ctx.identity.username}#`;
+    return{
+        operation : "GetItem",
+        key : {
+            PK: util.dynamodb.toDynamoDB(PK),
+            SK: util.dynamodb.toDynamoDB(SK)
+        }
+
+    };
+    
+}
+
+
+/**
+ * Called after the request function of the first AppSync function in the pipeline.
+ *  @param ctx the context object holds contextual information about the function invocation.
+ */
+
+export const response = (ctx) => {
+    if (ctx.result) {
+        return {
+            ...ctx.result,
+            
+        };
+    }
+};
