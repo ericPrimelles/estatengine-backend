7632317afa286394a1729a174d1a060f6f9d7401
ericPrimelles <eric.a.primelles96@gmail.com>
2025-06-19 21:22:21 -0400

    Fix function

--- appsync/backend_table/functions/createCustomer.js
+++ appsync/backend_table/functions/createCustomer.js
@@ -5,40 +5,24 @@ import {util} from "@aws-appsync/utils";
  *  @param ctx the context object holds contextual information about the function invocation.
  */
 
-function uuidv4() {
-    const bytes = crypto.getRandomValues(new Uint8Array(16));
-    
-    // Per RFC4122 standard
-    bytes[6] = (bytes[6] & 0x0f) | 0x40;
-    bytes[8] = (bytes[8] & 0x3f) | 0x80;
 
-    const hex = [...bytes].map(b => b.toString(16).padStart(2, '0'));
-
-    return [
-      hex.slice(0, 4).join(''),
-      hex.slice(4, 6).join(''),
-      hex.slice(6, 8).join(''),
-      hex.slice(8, 10).join(''),
-      hex.slice(10, 16).join('')
-    ].join('-');
-  }
 
 export function request(ctx) {
     const PK = `REALTOR#ACCOUNT#${ctx.identity?.username ?? "mockUser"}#CUSTOMER`;
-    const SK = `${ctx.arguments.input.customerType}#${uuidv4()}`;
-    x = {a : 1}
+    const SK = `${ctx.arguments.input.customerType}#${util.autoId()}`;
     
+    let atts = {};
+    for (const key in ctx.arguments.input) {
+        atts[key] = util.dynamodb.toDynamoDB(ctx.arguments.input[key]);
+    }
     return{
         operation : "PutItem",
         key : {
             PK: util.dynamodb.toDynamoDB(PK),
             SK: util.dynamodb.toDynamoDB(SK)
         },
-        attributeValues : Object.fromEntries(
-            Object.entries(ctx.arguments.input).map(([key, value]) => {
-                return [key, util.dynamodb.toDynamoDB(value)];
-            })
-        )
+        attributeValues : atts
+        
     };
     
 }
@@ -58,12 +42,9 @@ export const response = (ctx) => {
     }
     
     if (ctx.result) {
+        
         return {
-            data: Object.fromEntries(
-                Object.entries(ctx.result).map(([key, value]) => {
-                    return [key, util.dynamodb.toDynamoDB(value)];
-                })
-            ),
+            data: ctx.result,
             message: "Ok"
         };
     }
