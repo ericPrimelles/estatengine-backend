35893acbaa81d9da45ca9637013dd231916594ca
eric <eric.a.primelles96@gmail.com>
2025-06-26 14:50:49 -0400

    Creating search engine lambda

---
+++ src/search_engine/search_engine.py
@@ -0,0 +1,40 @@
+import os, requests
+
+def handler(event, context):
+    """
+    Lambda function handler to process search requests.
+    
+    Args:
+        event (dict): The event data containing search parameters.
+        context (object): The context object provided by AWS Lambda.
+        
+    Returns:
+        dict: A response containing the search results.
+    """
+    try:
+        GPSE_APIKERT = os.getenv('GPSE_APIKEY')
+        API_ID = os.getenv('GOOGLE_API_ID')
+        URL = 'https://www.googleapis.com/customsearch/v1'
+        max_results = event.get('max_results', 10)
+        search_query = event.get('query', '')
+
+        req = requests.get(
+            URL,
+            params={
+                'key': GPSE_APIKERT,
+                'cx': API_ID,
+                'q': search_query,
+                'num': max_results
+            }
+        )
+        results = req.json()
+        return {
+            'statusCode': 200,
+            'body': results
+        }
+    except Exception as e:
+        print(f"Error processing search request: {e}")
+        return {
+            'statusCode': 500,
+            'body': 'Internal Server Error'
+        }
